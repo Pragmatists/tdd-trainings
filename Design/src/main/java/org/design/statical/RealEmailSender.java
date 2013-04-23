c7fb65024e9c25dd9c5db473361d7432bd3b96dc
Tomasz Bartczak <none@none>
2013-04-23 12:01:42 +0200

    static emailer problem

---
+++ Design/src/main/java/org/design/statical/RealEmailSender.java
@@ -0,0 +1,39 @@
+package org.design.statical;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class RealEmailSender {
+
+	
+	
+	public static void sendEmailTo(String email, String subject) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "localhost");
+		props.put("mail.smtp.port", "2500");
+		Session session = Session.getDefaultInstance(props, null);
+
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("admin@example.com", "Example.com Admin"));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(email, "Mr. User"));
+            msg.setSubject(subject);
+            msg.setText("...");
+            Transport.send(msg);
+
+        } catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
